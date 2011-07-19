13daec208458ea40e06ec3b744fbb4ae51097dac
Graham Klyne <gk-github@ninebynine.org>
2011-07-19 11:00:48 +0100

    Created document template and HTML for consolidated tech requirements

--- docs/mkjson.sh
+++ docs/mkjson.sh
@@ -4,6 +4,10 @@
 #
 # @@TODO make part of dexy config
 #
+python ../python/ReadCSV.py UserRequirements-astro
+python ../python/ReadCSV.py UserRequirements-bio
+python ../python/ReadCSV.py UserRequirements-gen
+python ../python/ReadCSV.py TechRequirements-all
 python ../python/ReadCSV.py TechnicalFacets
 
 # End.
